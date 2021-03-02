6d1289ef96a22d6dcc580b459c27c4887836df71
khaled sidawi <khaledassa123@gmail.com>
2021-03-02 12:20:13 +0100

    done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,13 @@
 // Use this function that is linked to the button to change the background
 
 function set_background() {
-
+  theBody = document.getElementsByTagName("body")[0];
+  theParagraf = theBody.getElementsByTagName("p");
+  theFirst = theParagraf[0];
+  theFirst.style.background = 'yellow';
+  theSecond = theParagraf[1];
+  theSecond.style.background ='green';
+  theThird = theParagraf[2];
+  theThird.style.background = 'blue'; 
+   
 }
